9dd9188e6140156d3708628f58ec3817acc800ac
Vegas Cedric <vegascedric28@gmail.com>
2021-05-20 09:58:45 +0100

    peer connection integrated

--- app.js
+++ app.js
@@ -1,11 +1,25 @@
 const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+const { Server: io } = require('socket.io');
+
 const app = express();
 
-app.set('view engine', 'ejs');
 app.use(express.static('public'));
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-    res.render('room', { roomId: 1 });
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.params.room });
+});
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).broadcast.emit('user-connected', userId);
+    });
 });
 
 module.exports = app;
--- server.js
+++ server.js
@@ -1,9 +1,47 @@
-const app = require('./app');
-const http = require('http');
+// const app = require('./app');
+// const http = require('http');
+// const { ExpressPeerServer } = require('peer');
 
-const port = process.env.PORT || 3030;
-const host = process.env.HOST || 'localhost';
+// const port = process.env.PORT || 3030;
+// const host = process.env.HOST || 'localhost';
 
-const server = http.createServer(app);
+// const server = http.createServer(app);
 
-server.listen(port, () => console.log(`\nServer started on port http://${host}:${port}\n`));
+// const peerServer = ExpressPeerServer(server, { debug: true });
+// app.use('/peerjs', peerServer);
+
+// server.listen(port, () => console.log(`\nServer started on port http://${host}:${port}\n`));
+
+// server.js
+const express = require('express');
+const { v4: uuidv4 } = require('uuid');
+
+const app = express();
+const server = require('http').Server(app);
+
+app.set('view engine', 'ejs');
+
+const io = require('socket.io')(server);
+
+const { ExpressPeerServer } = require('peer');
+const peerServer = ExpressPeerServer(server, { debug: true });
+
+app.use('/peerjs', peerServer);
+app.use(express.static('public'));
+
+app.get('/', (req, res) => {
+    res.redirect(`/${uuidv4()}`);
+});
+
+app.get('/:room', (req, res) => {
+    res.render('room', { roomId: req.param.room });
+});
+
+io.on('connection', (socket) => {
+    socket.on('join-room', (roomId, userId) => {
+        socket.join(roomId);
+        socket.to(roomId).emit('user-connected', userId);
+    });
+});
+
+server.listen(3030);
